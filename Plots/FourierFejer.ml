74c1d73a5e97c1c2b3a50cfc7f4f2d0eccd72af4
turboNinja2 <julien.revelle@gmail.com>
2016-12-19 14:56:53 +0100

    Convergence plots for Fourier series

---
+++ Plots/FourierFejer.ml
@@ -0,0 +1,34 @@
+open Plplot
+module P = Plot
+
+let n = int_of_string(Sys.argv.(1)) ;;
+
+let cesaro_series x n f =
+  let rec partial_sums x n f sum = 
+    if n = 1 then sum else partial_sums x (n-1) f (sum +. (f x n))   
+  in (partial_sums x n f 0.) /. float_of_int(n+1) ;;
+
+
+let rec fourier_series_sawtooth x n =
+  let float_n = float_of_int(n) in
+  if n = 0 then 0. else (sin(float_n *. x)) /. float_n +. fourier_series_sawtooth x (n-1) ;;
+
+
+let f x = fourier_series_sawtooth x n ;;
+
+
+let g x = cesaro_series x n fourier_series_sawtooth ;;
+
+
+let simple_example g filename =
+  let p =
+    P.init (0.0, -2.0) (10.0, 2.0) `greedy (`svg `core) ~filename:filename
+  in
+  
+  P.plot ~stream:p [P.func `blue g (0.0, 10.0) ~step:0.001]; 
+  P.finish ~stream:p ();
+  ;;
+
+
+simple_example f ("fourier"^string_of_int(n)^".svg"); 
+simple_example g ("fejer"^string_of_int(n)^".svg");
---
+++ Plots/make.sh
@@ -0,0 +1,5 @@
+ocamlfind opt -package plplot -linkpkg -o fourier_fejer Sample2.ml
+
+rm *.cmi 
+rm *.cmx
+rm *.o
